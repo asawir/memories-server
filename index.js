ded522f626d6b4718a34089681383502d393036d
asawir <asawir@gmail.com>
2021-06-13 10:47:55 +0530

    make it better

--- index.js
+++ index.js
@@ -3,25 +3,23 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
-import dotenv, { config } from 'dotenv';
+import dotenv from 'dotenv';
 
 import postRoutes from './routes/posts.js';
 
 const app = express();
-dotenv.config();
 
 app.use(bodyParser.json({ limit: '30mb', extended: true }))
 app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
 app.use(cors());
 
 app.use('/posts', postRoutes);
+dotenv.config();
 
-app.get('/', ()=>{
-  res.send("Hello to Asawir Jinabade is first API")
-} )
-const PORT = process.env.PORT;
+const CONNECTION_URL = 'mongodb+srv://asawir:asawir123@cluster0.fjpcu.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+const PORT = process.env.PORT|| 5000;
 
-mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
   .catch((error) => console.log(`${error} did not connect`));
 
